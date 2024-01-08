4ba994e821649833b1c88e10a399353e71970da9
CosmeNicolas <nicomas7590@gmail.com>
2024-01-08 15:18:27 -0300

    agregue estilos al formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import {Container} from 'react-bootstrap'
-import reactLogo from './components/assets/react.svg'
+import TituloFormulario from './components/TituloFormulario'
 import Footer from './components/Footer'
+import Formulario from './components/Formulario';
 
 function App() {
  
@@ -10,13 +11,8 @@ function App() {
   return (
     <>
     <Container className='main '>
-      <div className='d-flex  justify-content-center mt-5'>
-          <img src={reactLogo} className="logo me-2 mb-2" alt="React logo" />
-      <h1 className='text-center'>
-        Formulario React
-      </h1>
-          <img src={reactLogo} className="logo ms-2 mb-2" alt="React logo" />
-      </div>
+      <TituloFormulario />
+      <Formulario/>
     </Container>
       <Footer/>
     </>
--- src/components/TituloFormulario.jsx
+++ src/components/TituloFormulario.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import reactLogo from './assets/react.svg'
 
 const TituloFormulario = () => {
   return (
-    <div>
-      
-    </div>
+      <div className='d-flex  justify-content-center mt-5'>
+        <img src={reactLogo} className="logo me-2 mb-2" alt="React logo" />
+        <h1 className='text-center'>
+          Formulario React
+        </h1>
+        <img src={reactLogo} className="logo ms-2 mb-2" alt="React logo" />
+      </div>
   );
 };
 
