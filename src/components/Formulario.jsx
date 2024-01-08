04493debab0aaeb3508fd88f0461dbd2266a17f4
CosmeNicolas <nicomas7590@gmail.com>
2024-01-08 18:04:57 -0300

    agregue el manejo de eventos y cambios q se producen en el formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,38 +1,87 @@
+import { useState } from 'react';
 import { Form , Button} from 'react-bootstrap';
 
 const Formulario = () => {
+
+  const [DatoFormulario, setDatoFormulario] = useState({nombre:'', apellido:'', dni:'',email:''});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setDatoFormulario({ ...DatoFormulario, [name]: value });
+  };
+  
+  const handleSubmit = (e)=>{
+    e.preventDeault();
+    console.log('desde el formulario')
+    const {nombre,apellido,dni,email} = DatoFormulario
+
+    if (nombre && apellido && dni && email) {
+      alert('Formulario enviado con exito')
+    } else {
+      alert('Completa todos los campos')
+    }
+  }
+
+
   return (
     <>
-    <div className='formulario-container  my-3'>
-      <Form className='py-5 px-3'>
+    <div className='formulario-container  my-4'>
+      <Form onSubmit={handleSubmit} className='py-5 px-3'>
         {/* nombre */}
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Nombre</Form.Label>
-          <Form.Control type="text" placeholder="Ingrese su Nombre" />
+          <Form.Control 
+          type="text"
+          placeholder="Ingrese su Nombre"
+          name='nombre'
+          minLength={2}
+          maxLength={50}
+          onChange={handleChange}
+          value={DatoFormulario.nombre}
+          required/>
         </Form.Group>
 
         {/* Apellido */}
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Apellido</Form.Label>
-          <Form.Control type="text" placeholder="Ingrese su Apellido" />
+          <Form.Control
+           type="text" 
+           placeholder="Ingrese su Apellido"
+           name='apellido'
+           onChange={handleChange}
+           value={DatoFormulario.apellido}
+           minLength={2}
+           maxLength={50}
+           required
+           />
         </Form.Group>
         {/* DNI */}
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Nro. DNI</Form.Label>
-          <Form.Control type="number" placeholder="Ingrese su DNI" />
+          <Form.Control
+           type="number" 
+           placeholder="Ingrese su DNI" 
+           name='dni'
+           onChange={handleChange}
+           value={DatoFormulario.dni}
+           required
+           />
         </Form.Group>
 
         {/* Email */}
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control 
+          type="email"
+          placeholder="Enter email" 
+          name='email'
+          maxLength={254}
+          onChange={handleChange}
+          value={DatoFormulario.email}
+          required/>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out"/>
-
-        </Form.Group>
-          <Button variant="dark" type="submit" className='m-0'>
+          <Button variant="dark" type="submit" className='mt-2'>
           Enviar Fomrulario
             <span></span>
             <span></span>
