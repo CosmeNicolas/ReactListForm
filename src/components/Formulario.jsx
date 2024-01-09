56fc88b9fd1c542e95341b132289c94ccca6af4d
CosmeNicolas <nicomas7590@gmail.com>
2024-01-08 22:10:15 -0300

    agregue las validaciones del formulario al enviar con su aler

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,26 +1,51 @@
 import { useState } from 'react';
-import { Form , Button} from 'react-bootstrap';
+import { Form , Button, Alert} from 'react-bootstrap';
 
 const Formulario = () => {
 
   const [DatoFormulario, setDatoFormulario] = useState({nombre:'', apellido:'', dni:'',email:''});
 
+  const [mostrarAlertaExitosa, setMostrarAlertaExitosa] = useState(false);
+  const [mostrarAlertaIncompleta, setMostrarAlertaIncompleta] = useState(false);
+  const [mostrarAlertaDNIInvalido, setMostrarAlertaDNIInvalido] = useState(false); // Estado y función para la alerta de DNI inválido
+  const initialState = { nombre: '', apellido: '', dni: '', email: '' };
+
+
   const handleChange = (e) => {
     const { name, value } = e.target;
+    console.log(DatoFormulario)
+
+    // Validar DNI
+    if (name === 'dni') {
+      if (!/^\d{0,8}$/.test(value)) {
+        setMostrarAlertaDNIInvalido(true); // Mostrar alerta si el DNI no cumple con las condiciones
+        return;
+      } else {
+        setMostrarAlertaDNIInvalido(false); // Ocultar alerta si el DNI es válido
+      }
+    }
+    
     setDatoFormulario({ ...DatoFormulario, [name]: value });
   };
   
   const handleSubmit = (e)=>{
-    e.preventDeault();
+    e.preventDefault();
     console.log('desde el formulario')
     const {nombre,apellido,dni,email} = DatoFormulario
 
+    
+
     if (nombre && apellido && dni && email) {
-      alert('Formulario enviado con exito')
+      setMostrarAlertaExitosa(true);
+      setMostrarAlertaIncompleta(false);
+      setDatoFormulario(initialState)
+      // Aquí puedes agregar lógica adicional para enviar los datos si es necesario
     } else {
-      alert('Completa todos los campos')
+      setMostrarAlertaExitosa(false);
+      setMostrarAlertaIncompleta(true);
     }
   }
+  
 
 
   return (
@@ -61,11 +86,18 @@ const Formulario = () => {
           <Form.Control
            type="number" 
            placeholder="Ingrese su DNI" 
+           minLength={8}
+           maxLength={8}
            name='dni'
            onChange={handleChange}
            value={DatoFormulario.dni}
            required
            />
+            {mostrarAlertaDNIInvalido && (
+              <Alert variant="danger" className='mt-1'>
+                El DNI debe contener exactamente 8 números.
+              </Alert>
+            )}
         </Form.Group>
 
         {/* Email */}
@@ -88,7 +120,20 @@ const Formulario = () => {
             <span></span>
             <span></span>
         </Button>
+        
       </Form>
+        {mostrarAlertaExitosa && (
+          <Alert variant="success" className='mt-3'>
+            ¡El formulario se ha enviado correctamente!
+          </Alert>
+        )}
+
+        {/* Alerta si faltan completar campos */}
+        {mostrarAlertaIncompleta && (
+          <Alert variant="danger" className='mt-3'>
+            Por favor, completa todos los campos del formulario.
+          </Alert>
+        )}
     </div>
  
     </>
